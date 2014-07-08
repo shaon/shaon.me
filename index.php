c4df4319768ab5313c3dc0008b3564c40c43cbe1
shaon <mdshaonimran@gmail.com>
2014-07-08 22:24:26 +0600

    initial commit

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <link rel="shortcut icon" href="http://www.shaon.me/favicon.ico" /> 
+        <meta charset="utf-8" />
+        <title>s h a o n . m e</title>
+        <link rel="stylesheet" href="css/reset.css" />
+        <link rel="stylesheet" href="css/text.css" />
+        <link rel="stylesheet" href="css/960.css" />
+        <link rel="stylesheet" href="css/main.css" />
+    </head>
+    <body>
+        <div class="container_16">
+
+            <div class="grid_16 center">
+                <img src="images/avatar.png" alt="Imran Hossain Shaon" />
+            </div>
+            <div class="clear"></div>
+            
+            <div class="grid_16">
+                <p class="name">Imran Hossain Shaon</p>
+                <hr />
+            </div>
+            <div class="clear"></div>
+
+            <div class="grid_16">
+                <p>gtalk: mdshaonimran</p>
+                <p>skype: mdshaonimran</p>
+                <p>email: mdshaonimran [at] gmail [dot] com</p>
+            </div>
+            <div class="clear"></div>
+            
+            <div class="grid_16 center social_container">
+                <a href="http://mdshaonimran.wordpress.com" target="_blank"><img class="social" src="images/wordpress.png" title="Blog" alt="wordpress" /></a>
+                <a href="http://www.twitter.com/mdshaonimran" target="_blank"><img class="social" src="images/twitter.png" title="Twitter" alt="twitter" /></a>
+                <a href="http://www.youtube.com/user/mdshaonimran" target="_blank"><img class="social" src="images/youtube.png" title="Youtube" alt="youtube" /></a>
+                <a href="http://www.linkedin.com/in/mdshaonimran" target="_blank"><img class="social" src="images/linkedin.png" title="LinkedIn" alt="linkedin" /></a>
+                <a href="http://www.slideshare.net/mdshaonimran" target="_blank"><img class="social" src="images/slideshare.png" title="Slideshare" alt="slideshare" /></a>
+                <a href="http://shaon.github.com" target="_blank"><img class="social_last" src="images/github.png" title="Github" alt="github" /></a>
+            </div>
+            <div class="clear"></div>
+        </div>
+    </body>
+</html>
